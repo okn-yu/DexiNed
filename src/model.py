5fdffaf40587a86799fe46cd8eb6d555667a6526
okn-yu <test@test.com>
2021-02-01 00:34:46 +0900

    updated.

--- src/model.py
+++ src/model.py
@@ -3,3 +3,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class _DenseLayer(nn.Sequential):
+    def __init__(self, input_features, out_features):
+        super(_DenseLayer, self).__init__()
+        self.add_module('conv1', nn.Conv2d(input_features, out_features, kernel_size=3, stride=1, padding=2, bias=True)),
+        self.add_module('norm1', nn.BatchNorm2d(out_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(out_features, out_features, kernel_size=3, stride=1, bias=True)),
+        self.add_module('norm2', nn.BatchNorm2d(out_features))
+
+    def forward(self, x):
+        x1, x2 = x
+        new_features = super(_DenseLayer, self).forward(F.relu(x1))
+        return 0.5 * (new_features + x2), x2
+
+
+class _DenseBlock(nn.Sequential):
+    def __init__(self, num_layers, input_features, out_features):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(input_features, out_features)
+            self.add_module('denselayer%d' % (i + 1), layer)
+            input_features = out_features
+
+
+# 1x1 convolution.
+class SingleConvBlock(nn.Module):
+    def __init__(self, in_features, out_features, stride, use_bn=True):
+        super(SingleConvBlock, self).__init__()
+        self.use_bs = use_bn
+        self.conv = nn.Conv2d(in_features, out_features, 1, stride=stride, bias=True)
+        self.bn = nn.BatchNorm2d(out_features)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.bn(x)
+
+        return x
+
+
+class DoubleConvBlock(nn.Module):
+    def __init__(self, in_features, mid_features, out_features=None, stride=1, use_act=True):
+        super(DoubleConvBlock, self).__init__()
+        self.use_act = use_act
+        if out_features is None:
+            out_features = mid_features
+        self.conv1 = nn.Conv2d(in_features, mid_features, 3, padding=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(mid_features)
+        self.conv2 = nn.Conv2d(mid_features, out_features, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_features)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        if self.use_act:
+            x = self.relu(x)
+
+        return x
+
+
+class DexiNed(nn.Module):
+
+    def __init_(self):
+        super(DexiNed, self).__init__()
+        self.block_1 = DoubleConvBlock(3, 32, 64, stride=2)
+        self.block_2 = DoubleConvBlock(64, 128, use_act=False)
+        self.dblock_3 = _DenseBlock(2, 128, 256)
+        self.dblock_4 = _DenseBlock(3, 256, 512)
+        self.dblock_5 = _DenseBlock(3, 512, 512)
+        self.dblock_6 = _DenseBlock(3, 512, 256)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
